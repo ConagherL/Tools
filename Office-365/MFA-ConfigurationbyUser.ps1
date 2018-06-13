3a3df0dd8b426602b558ad9d961f35faefdf231f
Conagher <23546026+ConagherL@users.noreply.github.com>
2018-06-13 11:16:05 -0500

    Update MFA-ConfigurationbyUser.ps1

--- Office-365/MFA-ConfigurationbyUser.ps1
+++ Office-365/MFA-ConfigurationbyUser.ps1
@@ -1,3 +1,18 @@
+#-----------------------------------------------------------------------------------
+# Author: Conagher Lepley
+# Blog: https://theitjunkie.com
+# Twitter: @conagherL
+# Date: 6/13/2018
+#-----------------------------------------------------------------------------------
+# Export MFA configuration of each user. MFA column may be blank if condtional access is used instead of turing on MFA directly in Azure
+#-----------------------------------------------------------------------------------
 
+#Connect to Azure AD
+$UserCredential = Get-Credential
+Connect-MsolService -Credential $UserCredential
+
+#Set file export path
 $filepath = "C:\temp\MFAUsers.csv"
-Get-MsolGroupMember -GroupObjectId "94da5047-6314-419a-bafd-a86f6a45124c" -MemberObjectTypes User -All | Get-MsolUser | Where {$_.UserPrincipalName} | Select UserPrincipalName, DisplayName, Country, Department, Title, @{n="MFA"; e={$_.StrongAuthenticationRequirements.State}}, @{n="Methods"; e={($_.StrongAuthenticationMethods).MethodType}}, @{n="Default Method"; e={($_.StrongAuthenticationMethods).IsDefault}} | Export-Csv -Path $filepath
+
+#Queries Azure AD for a sepcific group ID, colelcts the information, and exports to a file
+Get-MsolGroupMember -GroupObjectId "Populate group GUID ID Here" -MemberObjectTypes User -All | Get-MsolUser | Where {$_.UserPrincipalName} | Select UserPrincipalName, DisplayName, Country, Department, Title, @{n="MFA"; e={$_.StrongAuthenticationRequirements.State}}, @{n="Methods"; e={($_.StrongAuthenticationMethods).MethodType}}, @{n="Default Method"; e={($_.StrongAuthenticationMethods).IsDefault}} | Export-Csv -Path $filepath
