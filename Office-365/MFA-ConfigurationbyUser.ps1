2b4acbf4c484430a4484e815e61e8a381c7b0ce6
Conagher <23546026+ConagherL@users.noreply.github.com>
2018-06-13 11:16:36 -0500

    Update MFA-ConfigurationbyUser.ps1

--- Office-365/MFA-ConfigurationbyUser.ps1
+++ Office-365/MFA-ConfigurationbyUser.ps1
@@ -16,4 +16,3 @@ $filepath = "C:\temp\MFAUsers.csv"
 
 #Queries Azure AD for a sepcific group ID, colelcts the information, and exports to a file
 Get-MsolGroupMember -GroupObjectId "Populate group GUID ID Here" -MemberObjectTypes User -All | Get-MsolUser | Where {$_.UserPrincipalName} | Select UserPrincipalName, DisplayName, Country, Department, Title, @{n="MFA"; e={$_.StrongAuthenticationRequirements.State}}, @{n="Methods"; e={($_.StrongAuthenticationMethods).MethodType}}, @{n="Default Method"; e={($_.StrongAuthenticationMethods).IsDefault}} | Export-Csv -Path $filepath
-
